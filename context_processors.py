8dc6a82d30a5846800191d2a1e5acbd313f35811
Egor V. Nazarkin <nimnull@gmail.com>
2010-04-05 17:27:25 +0700

    доп. переменная текущего сайта

--- context_processors.py
+++ context_processors.py
@@ -1,9 +1,11 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
 
 
 def static(request):
     """Adds static media context variables to the context"""
     return {
+        'site': Site.objects.get(pk = settings.SITE_ID),
         'CSS_URL': settings.CSS_URL,
         'IMG_URL': settings.IMG_URL,
         'JS_URL': settings.JS_URL,
