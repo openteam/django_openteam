1e7c01f3886e460f994462dcecc6d1c94eb3802a
Egor Nazarkin <nimnull@gmail.com>
2011-03-23 17:11:56 +0600

    reverted dep

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author_email='nimnull@gmail.com',
     license='BSD',
     version='0.1.0',
+    install_requires=['Django','lxml'],
     py_modules=['openteam'],
     packages=find_packages(),
 )
