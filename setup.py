30878f7f9978d05e7f4d6e44669592deff212d8f
Egor Nazarkin <nimnull@gmail.com>
2011-03-23 17:03:35 +0600

    removed lxml dep

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     author_email='nimnull@gmail.com',
     license='BSD',
     version='0.1.0',
-    install_requires=['lxml'],
     py_modules=['openteam'],
     packages=find_packages(),
 )
